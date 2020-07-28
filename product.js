15fe5f2301c18abc28a6363cd9d870e0bfcfb9bf
Ardnh <ardn.h79@gmail.com>
2020-07-28 22:12:58 +0700

    Add event pada button & tambah data pada product.js

--- product.js
+++ product.js
@@ -3,9 +3,9 @@ new Vue({
     vuetify: new Vuetify(),
     data(){
         return {
-            cart: 0,
             show:false,
             overlay: false,
+            cart: [],
             items: [
                 { title: 'Lamp' },
                 { title: 'Chair' },
@@ -14,76 +14,80 @@ new Vue({
               ],
             products: [
                 {
+                    id: '1',
                     imgs:[
-                        {src:'img/7.jpg'},
-                        {src:'img/8.jpg'},
                         {src:'img/9.jpg'},
-                        {src:'img/10.jpg'},
-                        {src:'img/11.jpg'},
+                        {src:'img/8.jpg'},
                     ],
-                    title: 'A chair with four pillows four pillows',
+                    title: 'work desk for computer or laptop',
                     value: 5,
-                    sell:'270',
-                    price:'$150',
+                    sell:'390',
+                    price:'$450',
                     desc:'Lorem ipsum dolor sit, amet consectetur adicing elit. Tenetur excepturi fuga labore sit debitis nostrum.',
                     order:'Order Now',
-                    cart:'Add to Cart'
+                    cart:'Add to Cart',
+                    brand: 'ikea',
+                    region: 'Swiss'
                 },
                 {
+                    id: '2',
                     imgs:[
-                        {src:'img/7.jpg'},
-                        {src:'img/8.jpg'},
-                        {src:'img/9.jpg'},
                         {src:'img/10.jpg'},
-                        {src:'img/11.jpg'},
+                        {src:'img/8.jpg'},
                     ],
                     title: 'A chair with four pillows four pillows',
                     value: 3.5,
-                    sell:'270',
+                    sell:'120',
                     price:'$150',
                     desc:'Lorem ipsum dolor sit, amet consectetur adicing elit. Tenetur excepturi fuga labore sit debitis nostrum.',
                     order:'Order Now',
-                    cart:'Add to Cart'
+                    cart:'Add to Cart',
+                    brand: 'Bekant',
+                    region: 'US'
                 },
                 {
+                    id: '3',
                     imgs:[
-                        {src:'img/7.jpg'},
                         {src:'img/8.jpg'},
-                        {src:'img/9.jpg'},
-                        {src:'img/10.jpg'},
-                        {src:'img/11.jpg'},
+                        {src:'img/5.jpg'},
                     ],
-                    title: 'A chair with four pillows four pillows',
+                    title: 'relaxing sofa for in the family room',
                     value: 1.5,
-                    sell:'270',
-                    price:'$150',
+                    sell:'170',
+                    price:'$320',
                     desc:'Lorem ipsum dolor sit, amet consectetur adicing elit. Tenetur excepturi fuga labore sit debitis nostrum.',
                     order:'Order Now',
-                    cart:'Add to Cart'
+                    cart:'Add to Cart',
+                    brand: 'ikea',
+                    region: 'Swiss'
                 },
                 {
+                    id: '4',
                     imgs:[
                         {src:'img/7.jpg'},
                         {src:'img/8.jpg'},
-                        {src:'img/9.jpg'},
-                        {src:'img/10.jpg'},
-                        {src:'img/11.jpg'},
                     ],
-                    title: 'A chair with four pillows four pillows',
+                    title: 'flower vase for a more comfortable room',
                     value: 4.5,
-                    sell:'270',
-                    price:'$150',
+                    sell:'320',
+                    price:'$250',
                     desc:'Lorem ipsum dolor sit, amet consectetur adicing elit. Tenetur excepturi fuga labore sit debitis nostrum.',
                     order:'Order Now',
-                    cart:'Add to Cart'
+                    cart:'Add to Cart',
+                    brand: 'Olympic',
+                    region: 'Japan'
                 },
             ],
-
         }
     },
     methods:{
-        detail(){
-            alert("Detail akan dibuat")
-        }
+        // memasukan data ke dalam array cart
+        addToCart(product){
+            this.cart.push(product)
+
+        },
+        isInCart(product){
+            const productItem = this.cart.find(item => item.id === product.id)
+        } 
     }
   })
