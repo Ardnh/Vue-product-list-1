633c86c9623737f89a80f0654a7f249f4d5b1aef
Ardan Hilal <58196120+Ardnh@users.noreply.github.com>
2020-07-28 10:44:29 +0700

    Menambah file productlist.html & product.js

---
+++ product.js
@@ -0,0 +1,89 @@
+new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data(){
+        return {
+            cart: 0,
+            show:false,
+            overlay: false,
+            items: [
+                { title: 'Lamp' },
+                { title: 'Chair' },
+                { title: 'Sofa' },
+                { title: 'Bedroom' },
+              ],
+            products: [
+                {
+                    imgs:[
+                        {src:'img/7.jpg'},
+                        {src:'img/8.jpg'},
+                        {src:'img/9.jpg'},
+                        {src:'img/10.jpg'},
+                        {src:'img/11.jpg'},
+                    ],
+                    title: 'A chair with four pillows four pillows',
+                    value: 5,
+                    sell:'270',
+                    price:'$150',
+                    desc:'Lorem ipsum dolor sit, amet consectetur adicing elit. Tenetur excepturi fuga labore sit debitis nostrum.',
+                    order:'Order Now',
+                    cart:'Add to Cart'
+                },
+                {
+                    imgs:[
+                        {src:'img/7.jpg'},
+                        {src:'img/8.jpg'},
+                        {src:'img/9.jpg'},
+                        {src:'img/10.jpg'},
+                        {src:'img/11.jpg'},
+                    ],
+                    title: 'A chair with four pillows four pillows',
+                    value: 4.5,
+                    sell:'270',
+                    price:'$150',
+                    desc:'Lorem ipsum dolor sit, amet consectetur adicing elit. Tenetur excepturi fuga labore sit debitis nostrum.',
+                    order:'Order Now',
+                    cart:'Add to Cart'
+                },
+                {
+                    imgs:[
+                        {src:'img/7.jpg'},
+                        {src:'img/8.jpg'},
+                        {src:'img/9.jpg'},
+                        {src:'img/10.jpg'},
+                        {src:'img/11.jpg'},
+                    ],
+                    title: 'A chair with four pillows four pillows',
+                    value: 4.5,
+                    sell:'270',
+                    price:'$150',
+                    desc:'Lorem ipsum dolor sit, amet consectetur adicing elit. Tenetur excepturi fuga labore sit debitis nostrum.',
+                    order:'Order Now',
+                    cart:'Add to Cart'
+                },
+                {
+                    imgs:[
+                        {src:'img/7.jpg'},
+                        {src:'img/8.jpg'},
+                        {src:'img/9.jpg'},
+                        {src:'img/10.jpg'},
+                        {src:'img/11.jpg'},
+                    ],
+                    title: 'A chair with four pillows four pillows',
+                    value: 4.5,
+                    sell:'270',
+                    price:'$150',
+                    desc:'Lorem ipsum dolor sit, amet consectetur adicing elit. Tenetur excepturi fuga labore sit debitis nostrum.',
+                    order:'Order Now',
+                    cart:'Add to Cart'
+                },
+            ],
+
+        }
+    },
+    methods:{
+        detail(){
+            alert("Detail akan dibuat")
+        }
+    }
+  })
